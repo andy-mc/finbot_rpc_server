f2ffaed77d395081afbf42f4bad77ecb0225e584
Andrés Muñoz <asmz5488@gmail.com>
2017-07-16 18:48:42 -0500

    startUp finbot_rpc_server

---
+++ finbot_rpc_server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import json
+import pika
+from utils import finbot
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+                                     host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+
+def on_request(ch, method, props, body):
+
+    body = body.decode('utf-8')
+
+    print(" [.] finbot(%s)" % body)
+    response = finbot(body)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(
+                         correlation_id=props.correlation_id),
+                     body=json.dumps(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue')
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
